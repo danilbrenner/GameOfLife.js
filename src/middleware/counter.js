c40f5b25c0a51e183f077f3742a88b84976ccb3d
Danil Brenner <danilbrenner@outlook.com>
2018-05-24 20:40:21 +0300

    Add counter middleware

--- src/middleware/counter.js
+++ src/middleware/counter.js
@@ -1,10 +1,19 @@
-import { START_GAME, STOP_GAME } from '../actions/types';
+import { START_GAME, STOP_GAME, MAKE_STEP } from '../actions/types';
+
+let counter;
 
 export default store => next => action => {
     switch (action.type) {
     case START_GAME:
+        if(counter) break;
+        counter = setInterval(() => {
+            store.dispatch({ type: MAKE_STEP });
+        }, 300);
         break;
     case STOP_GAME:
+        if(!counter) break;
+        clearInterval(counter);
+        counter = undefined;
         break;
     }
     return next(action);
--- src/middleware/counter.test.js
+++ src/middleware/counter.test.js
@@ -1,8 +1,49 @@
 
+import counterMiddleware from './counter';
+import { START_GAME, STOP_GAME } from '../actions/types';
+
 describe('middleware', () => {
     describe('counter', () => {
-        it('any test', () => {
-            expect(1).toBe(1);
+        jest.useFakeTimers();
+
+        let store;
+        let next;
+
+        beforeEach(() => {
+            store = {
+                getState: jest.fn(() => ({})),
+                dispatch: jest.fn()
+            };
+            next = jest.fn();
         });
+
+        it('should start game counter', () => {
+
+            counterMiddleware(store)(next)({ type: START_GAME });
+            
+            expect(setInterval).toHaveBeenCalledTimes(1);
+            expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), 300);
+            expect(next).toHaveBeenCalledTimes(1);
+        });
+
+        it('should start game counter once', () => {
+
+            counterMiddleware(store)(next)({ type: START_GAME });
+            counterMiddleware(store)(next)({ type: START_GAME });
+            
+            expect(setInterval).toHaveBeenCalledTimes(1);
+            expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), 300);
+            expect(next).toHaveBeenCalledTimes(2);
+        });
+
+        it('should stop game counter', () => {
+
+            counterMiddleware(store)(next)({ type: START_GAME });
+            counterMiddleware(store)(next)({ type: STOP_GAME });
+            
+            expect(clearInterval).toHaveBeenCalledTimes(1);
+            expect(next).toHaveBeenCalledTimes(2);
+        });
+
     });
 });
